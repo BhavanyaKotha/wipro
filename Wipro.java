9bb4111271b7bfea685234e4d04001b8de8a0beb
BhavanyaKotha <kotha.bhavanya331@gmail.com>
2024-07-29 12:51:19 +0530

    msg

--- Wipro.java
+++ Wipro.java
@@ -3,7 +3,7 @@ package com.wipro;
 public class Wipro {
 	 public static void main(String[] args)
 	 {
-	    System.out.println("Hello World.......");	 
+	    System.out.println("Hello World.......welocme to java");	 
 	 }
 
 }
